16e5d973f1c39ec6a69c0c52711bfe33dfd733dd
Kursad Cengiz Yildirim <k.cengizyildirim@gmail.com>
2023-12-08 07:54:12 +0300

    Reader class created

---
+++ src/com/workintech/LibrarySystem/model/Reader.java
@@ -0,0 +1,10 @@
+package com.workintech.LibrarySystem.model;
+
+import com.workintech.LibrarySystem.enums.ReaderType;
+
+public class Reader {
+    private String name;
+    private long schoolNumber;
+    private String password;
+    private ReaderType readerType;
+}
