e9bfe9942435b3a085d28b3863e8a8f752cebd18
Kursad Cengiz Yildirim <k.cengizyildirim@gmail.com>
2023-12-08 07:50:39 +0300

    BookStatus and Categories enum created

---
+++ src/com/workintech/LibrarySystem/enums/BookStatus.java
@@ -0,0 +1,6 @@
+package com.workintech.LibrarySystem.enums;
+
+public enum BookStatus {
+    IN_STOCK,
+    OUT_OF_STOCK
+}
---
+++ src/com/workintech/LibrarySystem/enums/Categories.java
@@ -0,0 +1,10 @@
+package com.workintech.LibrarySystem.enums;
+
+public enum Categories {
+    ADVENTURE,
+    HORROR,
+    ROMANCE,
+    FANTASY,
+    SCIENCE_FICTION,
+    EDUCATION
+}
